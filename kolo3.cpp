1b42ed2c8fc70125d7de2387eb196bbf1eb5c8d1
Tomasz Sowa <snej88@gmail.com>
2011-06-10 11:34:57 +0200

    kom1

--- kolo3.cpp
+++ kolo3.cpp
@@ -1,10 +1,10 @@
-// Imie nazwisko, grupa(specjalnosc), nr. indeksu
+// Tomasz Sowa, ABiBD), 101581
 
 #include <iostream>
 using namespace std;
 
 int main(int argc, char **argv) {
-  cout<<"Program pobiera informacje od studenta i zapisuje je do pliku"<<endl;
+  cout<<"Program pobiera informacje od studenta i zapisuje je do plik"<<endl;
   return 0;
 }
 
